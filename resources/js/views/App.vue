da9c8c25258d40f6af4139517f2a745a333e5e4b
franciowel <spina_francesco@blu.it>
2022-09-06 17:39:47 +0200

    app vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Ciao sono la home della pagina pubblica
+    </h1>
+</template>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>
-        Home
-    </h1>
+    <div id="root">
+        
+    </div>
 @endsection
