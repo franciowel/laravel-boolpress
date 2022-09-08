8b8d408423898095c05a68322e8203ffe5a0317b
franciowel <spina_francesco@blu.it>
2022-09-08 16:44:41 +0200

    dashboard admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,16 @@
 @section('content')
     <div>
         <h1>
-            Dashboard privata
+            ADMIN PANEL
         </h1>
+
+        <div>
+            Welcome {{$user->name}}
+        </div>
+
+        <div>
+            La tua e-mail: {{$user->email}}
+        </div>
+
     </div>
 @endsection
