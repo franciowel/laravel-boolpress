0fdf9645ed1c5b83af115598d2dd35703b9cc2af
franciowel <spina_francesco@blu.it>
2022-09-09 16:33:11 +0200

    contenuti sashboard admin

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
     <h1>{{$post->title}}</h1>
+    <div>
+        Creato il : {{ $post->created_at }}
+    </div>
+
+    <div>
+        Aggiornato il : {{ $post->updated_at }}
+    </div>
+
+    <div>
+        Slug : {{ $post->slug }}
+    </div>
+
     <p>
         {{ $post->content }}
     </p>
